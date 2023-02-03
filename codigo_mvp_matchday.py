5edfac4041d56a66e2d00a4dc5b5a7d50ef2a641
pedrolacombes <124370288+pedrolacombes@users.noreply.github.com>
2023-02-03 12:06:49 -0300

    Update codigo_mvp_matchday.py

--- codigo_mvp_matchday.py
+++ codigo_mvp_matchday.py
@@ -631,7 +631,7 @@ with tab4:
 
   # puxando videos selecionados e fazendo upload online
   for video in lista_id_videos_selecionados:
-    video_file = open(https://github.com/pedrolacombes/matchday/tree/videos/{video}.mp4?raw=true', 'rb')
+    video_file = open('https://github.com/pedrolacombes/matchday/tree/videos/{video}.mp4?raw=true', 'rb')
     video_bytes = video_file.read()
     st.video(video_bytes)
 
