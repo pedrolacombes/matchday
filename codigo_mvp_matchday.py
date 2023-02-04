915544d168ca77a0ee7197500e3a9eea460710dc
pedrolacombes <124370288+pedrolacombes@users.noreply.github.com>
2023-02-04 12:44:55 -0300

    Update codigo_mvp_matchday.py

--- codigo_mvp_matchday.py
+++ codigo_mvp_matchday.py
@@ -632,6 +632,45 @@ with tab4:
 
   # puxando videos selecionados e fazendo upload online
   for video in lista_id_videos_selecionados:
-    video_file = cv2.VideoCapture(f'https://github.com/pedrolacombes/matchday/blob/main/1.mp4')
-    st.video(video_file)
+
+    # Create a VideoCapture object
+    cap = cv2.VideoCapture(f'https://github.com/pedrolacombes/matchday/blob/main/{video}.mp4?raw=true')
+ 
+    # Check if camera opened successfully
+    if (cap.isOpened() == False): 
+        print("Unable to read camera feed")
+ 
+    # Default resolutions of the frame are obtained.The default resolutions are system dependent.
+    # We convert the resolutions from float to integer.
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
+    out = cv2.VideoWriter('outpy.mp4',cv2.VideoWriter_fourcc(*'X264'), fps, (frame_width,frame_height))
+ 
+    while(True):
+        ret, frame = cap.read()
+ 
+        if ret == True: 
+     
+    # Write the frame into the file 'output.avi'
+            out.write(frame)
+    
+    # Press Q on keyboard to stop recording
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+ 
+    # Break the loop
+        else:
+            break 
+ 
+    # When everything done, release the video capture and video write objects
+    cap.release()
+    out.release()
+ 
+    # Closes all the frames
+    cv2.destroyAllWindows()
+
+    st.video('outpy.mp4')
 
