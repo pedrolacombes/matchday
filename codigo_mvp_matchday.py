ed0ba05a04d2f669d57292b2867da23d197d20fc
pedrolacombes <124370288+pedrolacombes@users.noreply.github.com>
2023-02-13 14:52:00 -0300

    Update codigo_mvp_matchday.py

--- codigo_mvp_matchday.py
+++ codigo_mvp_matchday.py
@@ -291,7 +291,7 @@ with tab3:
             if my_df['Nome_Pass_Outcome'][i] == 'Passe Errado':
                 ax.plot([int(my_df["x_start"][i]), int(my_df["x_end"][i])],[int(my_df["y_start"][i]), int(my_df["y_end"][i])], color="red")
                 ax.plot(int(my_df["x_end"][i]), int(my_df["y_end"][i]), "o", color="red")
-        st.write('setas vermelhas = passes errados')
+            ax.plot(0,26,'passe errado',color='blue')
 
         if option_stat_mapa == 'Finalizacoes':
             if my_df['Nome_Finalizacao_Outcome'][i] == 'Finalização bloqueada':
