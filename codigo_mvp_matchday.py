7e534b2269c43503c270c4b0d8ec85796a6220f7
pedrolacombes <124370288+pedrolacombes@users.noreply.github.com>
2023-02-13 15:33:06 -0300

    Update codigo_mvp_matchday.py

--- codigo_mvp_matchday.py
+++ codigo_mvp_matchday.py
@@ -290,10 +290,10 @@ with tab3:
 
         if option_stat_mapa == 'Passes':
             if my_df['Nome_Pass_Outcome'][i] == 'Passe Certo':
-                ax.plot([int(my_df["x_start"][i]), int(my_df["x_end"][i])],[int(my_df["y_start"][i]), int(my_df["y_end"][i])], color="blue")
+                ax.plot([int(my_df["x_start"][i]), int(my_df["x_end"][i])],[int(my_df["y_start"][i]), int(my_df["y_end"][i])], color="blue", linewidth =10)
                 ax.plot(int(my_df["x_end"][i]), int(my_df["y_end"][i]), "o", color="blue")
             if my_df['Nome_Pass_Outcome'][i] == 'Passe Errado':
-                ax.plot([int(my_df["x_start"][i]), int(my_df["x_end"][i])],[int(my_df["y_start"][i]), int(my_df["y_end"][i])], color="red")
+                ax.plot([int(my_df["x_start"][i]), int(my_df["x_end"][i])],[int(my_df["y_start"][i]), int(my_df["y_end"][i])], color="red", linewidth =10)
                 ax.plot(int(my_df["x_end"][i]), int(my_df["y_end"][i]), "o", color="red")
             ax.plot(0,26,'o',color='blue')
             ax.plot(0,27,'o',color='red')
