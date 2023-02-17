e9fae5655872cfc6a783ca51cb7f6d511beafb2b
pedrolacombes <124370288+pedrolacombes@users.noreply.github.com>
2023-02-17 17:03:41 -0300

    Update codigo_mvp_matchday.py

--- codigo_mvp_matchday.py
+++ codigo_mvp_matchday.py
@@ -523,7 +523,7 @@ with tab2:
     ax.add_patch(centreSpot)
 
   #Preenchendo de Verde
-    plt.fill_between([0,45],[25,25],color="Green", alpha=0.2)
+    plt.fill_between([0,45],[25,25],color="Green", alpha=0.4)
 
   # Tidy Axes
     ax.axis('off')
