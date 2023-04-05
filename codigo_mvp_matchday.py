4928199feda4d9c02192baf08b6ef5b6cddd906c
pedrolacombes <124370288+pedrolacombes@users.noreply.github.com>
2023-04-05 14:49:25 -0300

    Update codigo_mvp_matchday.py

--- codigo_mvp_matchday.py
+++ codigo_mvp_matchday.py
@@ -13,3 +13,13 @@ with st.container():
   url = 'https://www.youtube.com/watch?v=JK14AYu-wOs'
   st.video(url)
   st.write('Esse é um vídeo')
+
+with st.container():
+  url = 'https://www.youtube.com/watch?v=JK14AYu-wOs'
+  st.video(url)
+  st.write('Esse é um vídeo')
+
+with st.container():
+  url = 'https://www.youtube.com/watch?v=JK14AYu-wOs'
+  st.video(url)
+  st.write('Esse é um vídeo')
