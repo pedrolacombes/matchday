546f817455b078ec04c949f97f20f2e2fedd3d5d
pedrolacombes <124370288+pedrolacombes@users.noreply.github.com>
2023-04-05 17:03:09 -0300

    Update codigo_mvp_matchday.py

--- codigo_mvp_matchday.py
+++ codigo_mvp_matchday.py
@@ -12,11 +12,11 @@ import streamlit as st
 with st.container():
   url = 'https://www.youtube.com/watch?v=JK14AYu-wOs'
   st.video(url)
+  title = st.text_input('Deixe seu comentário','')
   expander = st.expander('Comentários')
   expander.write('Irado')
   expander.write('Muito legal')
-  title = st.text_input('Deixe seu comentário','')
-  title
+
   
     
   
