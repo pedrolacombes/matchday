6aa6965eb97732a2f51417aeae9ff41bc0043552
pedrolacombes <124370288+pedrolacombes@users.noreply.github.com>
2023-04-05 15:48:15 -0300

    Update codigo_mvp_matchday.py

--- codigo_mvp_matchday.py
+++ codigo_mvp_matchday.py
@@ -12,8 +12,8 @@ import streamlit as st
 with st.container():
   url = 'https://www.youtube.com/watch?v=JK14AYu-wOs'
   st.video(url)
-  expander = st.expander('Comentários')
-  expander.write('Irado')
-  expander.write('Muito legal')
+  #expander = st.expander('Comentários')
+  #expander.write('Irado')
+  #expander.write('Muito legal')
   st.button('Like')
-  
+  st.button('Comente')
